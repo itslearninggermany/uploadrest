994c3eed9d2f344be06618b8f0111581924f036e
nordmanii <thomas.nordmann@itslearning.com>
2020-05-28 12:19:42 +0200

    debug

--- univention.go
+++ univention.go
@@ -4,11 +4,12 @@ import "github.com/jinzhu/gorm"
 
 type univentionRestSetup struct {
 	gorm.Model
-	Uuid           string `gorm:"unique"`
-	OrganisationID string `gorm:"unique"`
-	InstitutionID  string
-	AESkey         string `gorm:"VARCHAR 500"`
-	AuthKey        string `gorm:"VARCHAR 500"`
+	FirstRun       bool   `gorm:"-" json:"first_run"`
+	Uuid           string `gorm:"unique" json:"uuid"`
+	OrganisationID string `gorm:"unique" json:"-"`
+	InstitutionID  string `json:"-"`
+	AESkey         string `gorm:"VARCHAR 500" json:"aeskey"`
+	AuthKey        string `gorm:"VARCHAR 500" json:"auth_key"`
 }
 
 func GetUniventionRestSetup(uuid string, db *gorm.DB) (setup *univentionRestSetup, err error) {
