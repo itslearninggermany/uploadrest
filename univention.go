64bc4dd8dc0596b9a04a5b57d90d0868321840c3
nordmanii <thomas.nordmann@itslearning.com>
2020-05-28 12:39:35 +0200

    debug

--- univention.go
+++ univention.go
@@ -12,11 +12,17 @@ type UniventionRestSetup struct {
 	AuthKey        string `gorm:"VARCHAR 500" json:"auth_key"`
 }
 
-func GetUniventionRestSetup(uuid string, db *gorm.DB) (setup UniventionRestSetup, err error) {
+func GetUniventionRestSetup(uuid string, db gorm.DB) (setup UniventionRestSetup, err error) {
 	err = db.Where("uuid = ?", uuid).Last(&setup).Error
 	return setup, err
 }
 
-func (p *UniventionRestSetup) Initial(AESKey string) {
+func (p *UniventionRestSetup) Initial(AESKey string) *UniventionRestSetup {
 	p.AESkey = AESKey
+	return p
+}
+
+func (p *UniventionRestSetup) Save(db gorm.DB) error {
+	err := db.Save(p)
+	return err
 }
