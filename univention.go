4bddf274d337556672f0affcf598ba3c1b33bede
nordmanii <thomas.nordmann@itslearning.com>
2020-05-28 12:23:07 +0200

    debug

--- univention.go
+++ univention.go
@@ -2,7 +2,7 @@ package uploadrest
 
 import "github.com/jinzhu/gorm"
 
-type univentionRestSetup struct {
+type UniventionRestSetup struct {
 	gorm.Model
 	FirstRun       bool   `gorm:"-" json:"first_run"`
 	Uuid           string `gorm:"unique" json:"uuid"`
@@ -12,12 +12,12 @@ type univentionRestSetup struct {
 	AuthKey        string `gorm:"VARCHAR 500" json:"auth_key"`
 }
 
-func GetUniventionRestSetup(uuid string, db *gorm.DB) (setup *univentionRestSetup, err error) {
-	err = db.Where("uuid = ?", uuid).Last(setup).Error
+func GetUniventionRestSetup(uuid string, db *gorm.DB) (setup UniventionRestSetup, err error) {
+	err = db.Where("uuid = ?", uuid).Last(&setup).Error
 	return setup, err
 }
 
-func (p *univentionRestSetup) Initial(AESKey string, authKey string) {
+func (p *UniventionRestSetup) Initial(AESKey string, authKey string) {
 	p.AESkey = AESKey
 	p.AuthKey = authKey
 }
