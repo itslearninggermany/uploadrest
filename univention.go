5fe2422b4005f8845bf00a775d2c06dcceba9296
nordmanii <thomas.nordmann@itslearning.com>
2020-05-28 14:09:49 +0200

    debug

--- univention.go
+++ univention.go
@@ -8,7 +8,7 @@ type UniventionRestSetup struct {
 	Uuid           string `gorm:"unique" json:"uuid"`
 	OrganisationID string `gorm:"unique" json:"-"`
 	InstitutionID  string `json:"-"`
-	AESkey         string `gorm:"VARCHAR 500" json:"aeskey"`
+	AESkey         []byte `gorm:"VARCHAR 500" json:"aeskey"`
 	AuthKey        string `gorm:"VARCHAR 500" json:"auth_key"`
 }
 
@@ -17,7 +17,7 @@ func GetUniventionRestSetup(uuid string, db *gorm.DB) (setup UniventionRestSetup
 	return setup, err
 }
 
-func (p *UniventionRestSetup) Initial(AESKey string) *UniventionRestSetup {
+func (p *UniventionRestSetup) Initial(AESKey []byte) *UniventionRestSetup {
 	p.AESkey = AESKey
 	return p
 }
